90504eb01adeee7ba9bfceda813e8dce033138f3
PauliinaV <vuorinen.pauliina@outlook.com>
2022-04-26 17:40:35 +0100

    add ordered factor

--- Functions/Functions_VariableTypeConversion.r
+++ Functions/Functions_VariableTypeConversion.r
@@ -1,12 +1,14 @@
 # Custom function: convert.magic
 # Function to convert multiple variables to different formats at once
 # Created by Bill Petti: https://gist.github.com/BillPetti/c83a471face493466fa3
+## This function has been edited with the addition of ordered = as.ordered
 
 convert.magic <- function(obj, type) {
     FUN1 <- switch(type,
         character = as.character,
         numeric = as.numeric,
-        factor = as.factor
+        factor = as.factor,
+        ordered = as.ordered
     )
     out <- lapply(obj, FUN1)
     as.data.frame(out)
