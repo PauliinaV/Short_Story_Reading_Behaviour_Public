8992b1453856acabde974db96d740017b4924c35
PauliinaV <vuorinen.pauliina@outlook.com>
2022-06-14 14:58:13 +0100

    add logical conversion type

--- Functions/Functions_VariableTypeConversion.r
+++ Functions/Functions_VariableTypeConversion.r
@@ -1,14 +1,16 @@
 # Custom function: convert.magic
 # Function to convert multiple variables to different formats at once
 # Created by Bill Petti: https://gist.github.com/BillPetti/c83a471face493466fa3
-## This function has been edited with the addition of ordered = as.ordered
+## This function has been edited with the addition of ordered = as.ordered and
+## logical = as.logical
 
 convert.magic <- function(obj, type) {
     FUN1 <- switch(type,
         character = as.character,
         numeric = as.numeric,
         factor = as.factor,
-        ordered = as.ordered
+        ordered = as.ordered,
+        logical = as.logical
     )
     out <- lapply(obj, FUN1)
     as.data.frame(out)
